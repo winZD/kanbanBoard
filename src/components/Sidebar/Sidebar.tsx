a07f0d3020aecea9f8af399bfdb14ff24e801ab9
winZD <puntica007@gmail.com>
2025-05-10 22:03:34 +0200

    feat: update Sidebar component to improve icon structure and replace unused imports

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,14 +1,48 @@
+// imports
 import { Menu } from "lucide-react";
-import Vector from "../../assets/Vector.svg";
+import home from "../../assets/Vector.svg";
 import logo from "../../assets/Logomark.png";
-import title from "../../assets/slothui.png";
+import stats from "../../assets/stats.svg";
+import s from "../../assets/s.svg";
+import slothui from "../../assets/slothui.png";
+import thunder from "../../assets/thunder.svg";
+import user from "../../assets/user.svg";
+import schedule from "../../assets/schedule.svg";
+import notification from "../../assets/notification.svg";
+import settings from "../../assets/settings.svg";
+import avatar from "../../assets/Avatar.png";
+
+// sidebar icon image array
+const sidebarIconsTop = [s, home, stats, user, schedule, thunder, notification];
+const sidebarIconsBottom = [settings, avatar];
 
 const Sidebar = () => {
   return (
     <>
-      <div className="hidden md:flex flex-col  p-4">
-        <div className="relative">
-          <img src={Vector} alt="" />
+      <div className="hidden md:flex flex-col p-4">
+        <div className="flex flex-col justify-between h-full items-center">
+          <div className="flex flex-col gap-5">
+            {sidebarIconsTop.map((icon, index) => (
+              <div key={index}>
+                <img
+                  src={icon}
+                  alt=""
+                  className={
+                    index === 0
+                      ? "bg-violet-800 rounded-xl bg-gradient-to-br from-purple-200 via-indigo-500 to-violet-700"
+                      : ""
+                  }
+                />
+              </div>
+            ))}
+          </div>
+          <div className="flex flex-col gap-3">
+            {sidebarIconsBottom.map((icon, index) => (
+              <div key={index}>
+                <img src={icon} alt="" />
+              </div>
+            ))}
+          </div>
         </div>
       </div>
 
@@ -16,7 +50,7 @@ const Sidebar = () => {
         <div className="flex flex-row justify-between">
           <div className="flex flex-row">
             <img src={logo} alt="" width={50} />
-            <img src={title} alt="" width={100} />
+            <img src={slothui} alt="" width={100} />
           </div>
           <Menu />
         </div>
