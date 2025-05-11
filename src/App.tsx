fdc029d8c020174ebe721c428438694c1818ccbb
winZD <puntica007@gmail.com>
2025-05-11 21:51:45 +0200

    banner

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import "./App.css";
+
 import Dashboard from "./components/Dashboard.tsx/Dashboard";
 import Header from "./components/Header/Header";
 import Sidebar from "./components/Sidebar/Sidebar";
@@ -13,6 +14,7 @@ function App() {
           <Header />
           <Dashboard />
         </div>
+        <OfflineBanner />
       </div>
     </TasksProvider>
   );
