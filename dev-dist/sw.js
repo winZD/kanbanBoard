f1f16dbab290f6367670c61424937a350aeda471
winZD <puntica007@gmail.com>
2025-05-13 15:27:55 +0200

    fix: update README and service worker with consistent formatting and project setup instructions

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-e755d862'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.376dcpbvli"
+    "revision": "0.07ju1nucqu"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
